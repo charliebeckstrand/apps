24762240525a02a470cb81da8436c39531108e5a
Charlie Beckstrand <charlie.beckstrand@icloud.com>
2024-08-22 19:19:07 -0600

    updated lintstagedrc and scriptlint config files

--- lint-staged.config.js
+++
@@ -1,8 +0,0 @@
-const config = {
-	'./package.json': ['scriptlint --fix'],
-	'**/*.{js,json}': ['prettier --write'],
-	'**/*.{vue,ts}': ['eslint --cache --no-ignore --fix --max-warnings=0', 'prettier --write'],
-	'packages/**/package.json': ['scriptlint --fix']
-}
-
-export default config
